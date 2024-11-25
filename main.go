0ddb334bc6624fb13307b45956b26cf685f97d00
Nathanael <natnaeltamirat12@gmail.com>
2024-11-25 20:45:43 +0800

    changed to port 8080

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		}
 	})
 	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8065", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
