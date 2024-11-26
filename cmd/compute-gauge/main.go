da668612d19f8b80c3473c308f2f47e0af30292e
Nathanael <natnaeltamirat12@gmail.com>
2024-11-26 15:13:22 +0800

    local entrypoint

---
+++ cmd/compute-gauge/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"compute-gauge/internal/handlers"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		path = strings.TrimSuffix(path, "/")
+		if strings.HasPrefix(path, "/static/") {
+			handlers.HandleStatic(w, r, "/static/")
+			return
+		}
+		switch path {
+		case "", "/index", "/index.html":
+			handlers.HandleIndex(w, r)
+		case "/api/calculate":
+			handlers.HandleCalculate(w, r)
+		case "/documentation":
+			handlers.HandleDocs(w, r)
+		default:
+			http.NotFound(w, r)
+		}
+	})
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
