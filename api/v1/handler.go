b51ee125260f8bd97feb313206c35cb93ddd2a2a
Nathanael <natnaeltamirat12@gmail.com>
2024-11-26 15:12:47 +0800

    vercel entrypoint

---
+++ api/v1/handler.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"compute-gauge/internal/handlers"
+	"net/http"
+	"strings"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	path = strings.TrimSuffix(path, "/")
+	if strings.HasPrefix(path, "/static/") {
+		handlers.HandleStatic(w, r, "/")
+		return
+	}
+	switch path {
+	case "", "/index", "/index.html":
+		handlers.HandleIndex(w, r)
+	case "/api/calculate":
+		handlers.HandleCalculate(w, r)
+	case "/documentation":
+		handlers.HandleDocs(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
